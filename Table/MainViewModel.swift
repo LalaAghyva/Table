dbb9d92ef371c2b6d6186fd7167f3a7e146e1242
Lala <laleagayeva78@gmail.com>
2024-02-21 03:40:49 +0400

    Table

---
+++ Table/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  mainViewModel.swift
+//  Table
+//
+//  Created by Lala on 21.02.24.
+//
+
+import Foundation
---
+++ Table/SeconCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SeconCellTableViewCell.swift
+//  Table
+//
+//  Created by Lala on 21.02.24.
+//
+
+import UIKit
+
+class SeconCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
